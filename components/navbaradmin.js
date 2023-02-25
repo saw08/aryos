34b8d5e2ae646c682aac12c10d50a49c45d1ae0b
saw08 <kuliah.1182@gmail.com>
2023-02-26 00:32:28 +0700

    firstdddjsbaax

--- components/navbaradmin.js
+++ components/navbaradmin.js
@@ -7,7 +7,7 @@ function navbarAdmin() {
     return (
         < Navbar id="header" className="navbar-dark d-flex align-items-cente" expand="lg">
             <Container className='container-fluid container-xl d-flex align-items-center justify-content-lg-between'>
-                <Navbar.Brand href="/admin/home">
+                {/* <Navbar.Brand href="/admin/home">
 
                     <img
                         className='logo me-auto me-lg-0'
@@ -16,13 +16,13 @@ function navbarAdmin() {
                         width="100"
                         height="100"
                     />{'  '}
-                    {/* <h4 className='text-white text-start p-2'>Warung kemarang</h4> */}
-                </Navbar.Brand>
+                    <h4 className='text-white text-start p-2'>Warung kemarang</h4>
+                </Navbar.Brand> */}
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="navbar-header " >
-                        <Nav.Link className='nav-link scrollto' href="../">
-                            <h4 className='text-white text-center'><u>ADMIN ARYOS GRUP</u></h4>
+                        <Nav.Link className='nav-link scrollto' href="../admin/home" style={{ borderStyle: 'solid', borderColor: 'white', borderRadius: '0.3rem' }}>
+                            <h4 className='text-white text-center' ><u>ADMIN ARYOS GRUP</u></h4>
                         </Nav.Link>
                         <Nav.Link className='nav-link scrollto' href="../admin/home">
                             <h5 className='text-white text-start'>Home</h5>
