bfe912e7e14729870f1e38d43612d40b6ec51818
saw08 <kuliah.1182@gmail.com>
2023-02-27 19:50:53 +0700

    xxx

--- components/home/weding/album.js
+++ components/home/weding/album.js
@@ -20,7 +20,7 @@ export default function Home() {
         <section id="gallery" className="gallery">
             <div className="container" data-aos="fade-up">
                 <div className="section-title">
-                    <h2>WEDING & PRAWEDING</h2>
+                    <h2>WEDDING & PRAWEDDING</h2>
                 </div>
             </div>
             <div className="container-fluid" data-aos="fade-up">
