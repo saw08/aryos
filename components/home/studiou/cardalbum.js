ac6c331222bec6435939a9b37924be898002164c
saw08 <kuliah.1182@gmail.com>
2023-02-27 21:29:47 +0700

    xxxssds

--- components/home/studiou/cardalbum.js
+++ components/home/studiou/cardalbum.js
@@ -3,10 +3,10 @@ import Link from 'next/link';
 export default function Cardpesanan({ props }) {
     
     return (
-        <div className="col-lg-3 col-md-4">
+        <div className="col-lg-6 col-md-10">
             <div className=''>
                 <div className="profile-card-6">
-                    <img src={props.foto} className="img img-fluid" height={300} width={300} style={{ objectFit: "cover" }} />
+                    <img src={props.foto} className="img img-fluid" height={500} width={300} style={{ objectFit: "cover" }} />
                     <div className="profile-name">{` ${props.nama}`}</div>
                 </div>
             </div>
